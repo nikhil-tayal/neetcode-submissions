0c05389543a98ca3c60e56447560e7c0b0d770f3
Nikhil Tayal <45423111+nikhil-tayal@users.noreply.github.com>
2026-05-08 03:44:18 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let min = Infinity;
+        let max = 0;
+        for (let i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                max = Math.max(max, prices[i] - min);
+            }
+        }
+
+        console.log(min);
+        return max
+    }
+}
